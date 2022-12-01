f2ac025325963105e8c434ccf61eb16018151992
Mikel-Graf <116977745+Mikel-Graf@users.noreply.github.com>
2022-12-01 08:16:32 +0100

    aktuelles Version

--- news.php
+++ news.php
@@ -82,9 +82,9 @@
       </div>
       <!-- HERO -->
       <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Neuigkeiten</h1>
+        <h1 class="display-5 fw-bold">Trainingszeiten</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">Hier wird irgendwann mehr Inhalt stehen</p>
+          <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
         </div>
       </div>
       <!-- FOOTER -->
